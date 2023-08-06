1769585bd07a1a4e9381819f1f9c8b918c4e31ab
DawnSouther <571863559@qq.com>
2023-08-06 10:09:20 +0000

    add: 添加方法到http的映射逻辑

--- packages/musubi/src/lib/method-sigature.util.ts
+++ packages/musubi/src/lib/method-sigature.util.ts
@@ -1,19 +1,42 @@
-interface RequestMappingConifg{
-  method: 'DELETE' | 'GET' | 'POST' | 'PUT';
+type Method = 'DELETE' | 'GET' | 'POST' | 'PUT';
+type ParamType = 'QUERY' | 'BODY';
+
+interface RequestMappingConifg {
+  method: Method;
   path: string;
-  paramType: 'QUERY' | 'BODY'
+  paramType: ParamType;
 }
 
-export function methodToHttp(name: string): RequestMappingConifg{
-  // ! TODO 完善名称转换逻辑
-  return {
-    method: 'GET',
-    path: name,
-    paramType: 'QUERY'
+const GET_MAPPING = ['find', 'query', 'select', 'list', 'get'];
+const POST_MAPPING = ['create'];
+const PUT_MAPPING = ['modify', 'update'];
+const DELETE_MAPPING = ['delete', 'remove', 'del'];
+
+export function methodToHttp(name: string): RequestMappingConifg {
+  const lowerName = name.toLowerCase();
+  let method: Method = 'POST';
+  let paramType: ParamType = 'BODY';
+  if (GET_MAPPING.includes(lowerName)) {
+    method = 'GET';
+    paramType = 'QUERY';
+  } else if (POST_MAPPING.includes(lowerName)) {
+    method = 'POST';
+    paramType = 'BODY';
+  } else if (PUT_MAPPING.includes(lowerName)) {
+    method = 'PUT';
+    paramType = 'BODY';
+  } else if (DELETE_MAPPING.includes(lowerName)) {
+    method = 'DELETE';
+    paramType = 'BODY';
   }
+  return {
+    method,
+    path: name,
+    paramType,
+  };
 }
 
 export function extractMethodParams(method: Function): string[] {
   const methodStr = method.toString();
-  return methodStr.slice(methodStr.indexOf('(') + 1, methodStr.indexOf(')')).match(/([^\s,]+)/g)??[];
+  return methodStr.slice(methodStr.indexOf('(') + 1, methodStr.indexOf(')')).match(/([^\s,]+)/g) ?? [];
 }
