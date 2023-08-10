9d54052edc831beb257fead813c6a74db356a861
DawnSouther <571863559@qq.com>
2023-08-10 14:41:53 +0000

    remove: 移除废弃代码

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -9,13 +9,6 @@ export class AppComponent implements OnInit, AfterViewInit {
   constructor(private preloader: PreloaderService, private settings: SettingsService) {}
 
   ngOnInit() {
-    // // @ts-ignore
-    // if (module.hot) {
-    //   // @ts-ignore
-    //   module.hot.accept('./README.md', () => {
-    //   console.log('热替换');
-    // });
-    // }
     this.settings.setDirection();
     this.settings.setTheme();
   }
