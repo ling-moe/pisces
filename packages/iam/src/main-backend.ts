43e966a2522b84e378b38f3a41c07fb990fa7de4
yukdawn <yukdawn@gmail.com>
2023-11-30 10:40:42 +0800

    add: 添加swc编译

--- packages/iam/src/main-backend.ts
+++ packages/iam/src/main-backend.ts
@@ -4,7 +4,6 @@ import { CoreBackendModule } from '@pisces/backend';
 import * as bodyParser from 'body-parser';
 import { BigIntModule, initStandard } from '@pisces/common';
 import { IamBackendModule } from './app/iam.backend.module';
-import * as ViteExpress from "vite-express";
 
 @Module({
   imports: [
@@ -21,8 +20,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3100;
-  const server = await app.listen(port);
-  ViteExpress.bind(app.getHttpAdapter().getInstance(), server);
+  await app.listen(port);
   Logger.log(
     `🚀 IamApplication is running on: http://localhost:${port}/${globalPrefix}`
   );
