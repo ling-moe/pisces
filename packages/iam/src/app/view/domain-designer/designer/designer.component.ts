cf768e5a51311e41d0f7bdb6c7acfc93a1385477
yukdawn <yukdawn@gmail.com>
2024-03-03 14:50:09 +0800

    add: 尝试使用文档编辑器

--- packages/iam/src/app/view/domain-designer/designer/designer.component.ts
+++ packages/iam/src/app/view/domain-designer/designer/designer.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { createEmptyPage, DocEditor } from "@blocksuite/presets";
 
 @Component({
   selector: 'pisces-designer',
@@ -18,7 +19,59 @@ export class DesignerComponent implements OnInit {
 
   ngOnInit(): void {
     this.productId =this.route.snapshot.params['id'];
-    console.log(this.productId)
+    const editor = new DocEditor();
+    const doc = createEmptyPage().init();
+    editor.page = doc;
+    const {docModeSpecs, edgelessModeSpecs} = getSpecs()
+    editor.specs = docModeSpecs;
+    this.container.nativeElement.appendChild(editor);
+
+    // The first block will be added as root
+    const rootId = doc.addBlock('affine:page');
+
+// Insert second block as a child of the root with empty props
+    const props = {};
+    const noteId = doc.addBlock('affine:note', props, rootId);
   }
 
 }
+
+
+import { DocEditorBlockSpecs, EdgelessEditorBlockSpecs, DocPageService, EdgelessPageService } from '@blocksuite/blocks';
+
+class CustomDocPageService extends DocPageService {
+  override loadFonts() {}
+}
+
+class CustomEdgelessPageService extends EdgelessPageService {
+  override loadFonts() {}
+}
+
+export const getSpecs = () => {
+  const docModeSpecs = DocEditorBlockSpecs.map((preset) => {
+    if (preset.schema.model.flavour === 'affine:page') {
+      return {
+        ...preset,
+        service: CustomDocPageService,
+      };
+    }
+
+    return preset;
+  });
+
+  const edgelessModeSpecs = EdgelessEditorBlockSpecs.map((preset) => {
+    if (preset.schema.model.flavour === 'affine:page') {
+      return {
+        ...preset,
+        service: CustomEdgelessPageService,
+      };
+    }
+
+    return preset;
+  });
+
+  return {
+    docModeSpecs,
+    edgelessModeSpecs,
+  };
+};
