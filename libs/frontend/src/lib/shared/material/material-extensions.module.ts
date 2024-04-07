077517d9454a08671c44d59cac38326de41d462a
dawn-book <yukdawn@gmail.com>
2024-04-07 12:31:13 +0800

    update: 更新matro到17

--- libs/frontend/src/lib/shared/material/material-extensions.module.ts
+++ libs/frontend/src/lib/shared/material/material-extensions.module.ts
@@ -7,14 +7,13 @@ import { MtxColorpickerModule } from '@ng-matero/extensions/colorpicker';
 import { MtxDatetimepickerModule } from '@ng-matero/extensions/datetimepicker';
 import { MtxDialogModule } from '@ng-matero/extensions/dialog';
 import { MtxDrawerModule } from '@ng-matero/extensions/drawer';
-// import { MtxGridModule } from '@ng-matero/extensions/grid';
+import { MtxGridModule } from '@ng-matero/extensions/grid';
 import { MtxLoaderModule } from '@ng-matero/extensions/loader';
 import { MtxPopoverModule } from '@ng-matero/extensions/popover';
 import { MtxProgressModule } from '@ng-matero/extensions/progress';
 import { MtxSelectModule } from '@ng-matero/extensions/select';
-// import { MtxSliderModule } from '@ng-matero/extensions/slider';
 import { MtxSplitModule } from '@ng-matero/extensions/split';
-// import { MtxTooltipModule } from '@ng-matero/extensions/tooltip';
+import { MtxTooltipModule } from '@ng-matero/extensions/tooltip';
 import { MTX_DATETIME_FORMATS } from '@ng-matero/extensions/core';
 import { MtxMomentDatetimeModule } from '@ng-matero/extensions-moment-adapter';
 
@@ -27,14 +26,13 @@ import { MtxMomentDatetimeModule } from '@ng-matero/extensions-moment-adapter';
     MtxDatetimepickerModule,
     MtxDialogModule,
     MtxDrawerModule,
-    // MtxGridModule,
+    MtxGridModule,
     MtxLoaderModule,
     MtxPopoverModule,
     MtxProgressModule,
     MtxSelectModule,
-    // MtxSliderModule,
     MtxSplitModule,
-    // MtxTooltipModule,
+    MtxTooltipModule,
     MtxMomentDatetimeModule, // <= You can import the other adapter you need (e.g. luxon, date-fns)
   ],
   providers: [
