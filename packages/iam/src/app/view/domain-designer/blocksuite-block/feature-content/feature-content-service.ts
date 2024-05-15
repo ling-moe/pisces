ad318bab1bdf21604601bb38cf77a66243e9ef7c
dawn-book <yukdawn@gmail.com>
2024-05-15 15:02:44 +0800

    fix: 整理项目

--- packages/iam/src/app/view/domain-designer/blocksuite-block/feature-content/feature-content-service.ts
+++ packages/iam/src/app/view/domain-designer/blocksuite-block/feature-content/feature-content-service.ts
@@ -1,23 +1,10 @@
 import { BlockService } from '@blocksuite/block-std';
-import { AffineTextAttributes, InlineManager, ReferenceNodeConfig, getAffineInlineSpecsWithReference } from '@blocksuite/blocks';
-import { affineInlineMarkdownMatches } from '@blocksuite/blocks/dist/_common/inline/presets/markdown.js';
 import { FeatureContentBlockModel } from './feature-content-model';
 
-export class FeatureContentService<
-  TextAttributes extends AffineTextAttributes = AffineTextAttributes,
-> extends BlockService<FeatureContentBlockModel> {
-  readonly inlineManager = new InlineManager<TextAttributes>();
-  readonly referenceNodeConfig = new ReferenceNodeConfig();
+export class FeatureContentService extends BlockService<FeatureContentBlockModel> {
 
   override mounted(): void {
     super.mounted();
 
-    this.referenceNodeConfig.setDoc(this.doc);
-
-    const inlineSpecs = getAffineInlineSpecsWithReference(
-      this.referenceNodeConfig
-    );
-    this.inlineManager.registerSpecs(inlineSpecs);
-    this.inlineManager.registerMarkdownMatches(affineInlineMarkdownMatches);
   }
 }
--- packages/iam/src/app/view/domain-designer/blocksuite-block/feature/Feature-service.ts
+++ packages/iam/src/app/view/domain-designer/blocksuite-block/feature/Feature-service.ts
@@ -1,23 +1,9 @@
 import { BlockService } from '@blocksuite/block-std';
-import { AffineTextAttributes, InlineManager, ReferenceNodeConfig, getAffineInlineSpecsWithReference } from '@blocksuite/blocks';
-import { affineInlineMarkdownMatches } from '@blocksuite/blocks/dist/_common/inline/presets/markdown.js';
 import { FeatureBlockModel } from './feature-model';
 
-export class FeatureService<
-  TextAttributes extends AffineTextAttributes = AffineTextAttributes,
-> extends BlockService<FeatureBlockModel> {
-  readonly inlineManager = new InlineManager<TextAttributes>();
-  readonly referenceNodeConfig = new ReferenceNodeConfig();
+export class FeatureService extends BlockService<FeatureBlockModel> {
 
   override mounted(): void {
     super.mounted();
-
-    this.referenceNodeConfig.setDoc(this.doc);
-
-    const inlineSpecs = getAffineInlineSpecsWithReference(
-      this.referenceNodeConfig
-    );
-    this.inlineManager.registerSpecs(inlineSpecs);
-    this.inlineManager.registerMarkdownMatches(affineInlineMarkdownMatches);
   }
 }
