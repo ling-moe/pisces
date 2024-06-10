b0d0981fce39144b296315ec5385fe6e1ab981a3
dawn-book <yukdawn@gmail.com>
2024-06-10 13:24:37 +0800

    add: 管家领域管理菜单

--- packages/iam/src/app/infra/config/iam.module.frontend.ts
+++ packages/iam/src/app/infra/config/iam.module.frontend.ts
@@ -1,60 +1,60 @@
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatTreeModule } from "@angular/material/tree";
-import { Route, RouterModule } from "@angular/router";
-import { SharedModule } from "@pisces/frontend";
-import { MenuListComponent } from "../../view/menu/list/list.component";
-import { TransferComponent } from "../../view/menu/transfer/transfer.component";
-import { RoleListComponent } from "../../view/role/list/list.component";
-import { RoleAuthorizationComponent } from "../../view/role/role-authorization/role-authorization.component";
-import { UserAssignComponent } from "../../view/role/user-assign/user-assign.component";
-import { UserCreateComponent } from "../../view/user/create/create.component";
-import { UserEditComponent } from "../../view/user/edit/edit.component";
-import { UserListComponent } from "../../view/user/list/list.component";
-import { DesignerComponent } from "../../view/domain-designer/designer/designer.component";
-import { ProductComponent } from "../../view/domain-designer/product/product.component";
-import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+import { Route, RouterModule } from '@angular/router';
+import { SharedModule } from '@pisces/frontend';
+import { MenuListComponent } from '../../view/menu/list/list.component';
+import { TransferComponent } from '../../view/menu/transfer/transfer.component';
+import { RoleListComponent } from '../../view/role/list/list.component';
+import { RoleAuthorizationComponent } from '../../view/role/role-authorization/role-authorization.component';
+import { UserAssignComponent } from '../../view/role/user-assign/user-assign.component';
+import { UserCreateComponent } from '../../view/user/create/create.component';
+import { UserEditComponent } from '../../view/user/edit/edit.component';
+import { UserListComponent } from '../../view/user/list/list.component';
+import { DesignerComponent } from '../../view/domain-designer/designer/designer.component';
+import { ProductComponent } from '../../view/domain-designer/product/product.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { DomainComponent } from '../../view/domain-designer/domain/domain.component';
 
-const COMPONENTS = [
-  UserListComponent,
-  UserCreateComponent,
-  UserEditComponent,
-  MenuListComponent];
+const COMPONENTS = [UserListComponent, UserCreateComponent, UserEditComponent, MenuListComponent];
 
 const iamFrontendRoutes: Route[] = [
   {
-    path: "domain",
+    path: 'domain',
     children: [
       {
-        path: "product",
-        component: ProductComponent
+        path: 'product',
+        component: ProductComponent,
       },
       {
-        path: "designer/:id",
-        component: DesignerComponent
-      }
-    ]
+        path: 'domain',
+        component: DomainComponent,
+      },
+      {
+        path: 'designer/:id',
+        component: DesignerComponent,
+      },
+    ],
   },
   {
-    path: "",
+    path: '',
     children: [
       {
-        path: "user",
-        component: UserListComponent
+        path: 'user',
+        component: UserListComponent,
       },
       {
-        path: "role",
-        component: RoleListComponent
+        path: 'role',
+        component: RoleListComponent,
       },
       {
-        path: "menu",
-        component: MenuListComponent
-      }
-    ]
-  }
-
+        path: 'menu',
+        component: MenuListComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
@@ -65,7 +65,7 @@ const iamFrontendRoutes: Route[] = [
     MatTreeModule,
     MatIconModule,
     MatButtonModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
   ],
   declarations: [
     ...COMPONENTS,
@@ -75,7 +75,7 @@ const iamFrontendRoutes: Route[] = [
     UserAssignComponent,
     ProductComponent,
     DesignerComponent,
-  ]
+    DomainComponent,
+  ],
 })
-export class IamModuleFrontend {
-}
+export class IamModuleFrontend {}
---
+++ packages/iam/src/app/view/domain-designer/domain/domain.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pisces-domain',
+  templateUrl: './domain.component.html',
+  styleUrl: './domain.component.scss',
+})
+export class DomainComponent {}
