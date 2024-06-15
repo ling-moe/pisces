48885ac433dc0838397dc9219f04755a073a0c8b
dawn-book <yukdawn@gmail.com>
2024-06-15 10:47:17 +0800

    fix

--- packages/iam/src/app/infra/config/prisma.module.backend.ts
+++ packages/iam/src/app/infra/config/prisma.module.backend.ts
@@ -1,10 +1,8 @@
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import { Prisma, PrismaClient } from "../../../../../../dist/prisma/iam";
+import { Prisma, PrismaClient } from "../prisma";
 import { createPrismaService } from "@pisces/backend";
 
 export const PrismaService = createPrismaService<Prisma.ModelName, typeof PrismaClient>(PrismaClient, Prisma);
 
 export type PrismaService = InstanceType<typeof PrismaService>;
 
-// eslint-disable-next-line @nx/enforce-module-boundaries
-export * from "../../../../../../dist/prisma/iam";
+export * from "../prisma";
