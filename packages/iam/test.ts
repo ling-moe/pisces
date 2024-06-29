9d82462dceea82de1e3db83275d55b29b5432a8c
dawn-book <yukdawn@gmail.com>
2024-06-29 18:43:44 +0800

    update: 更新tsconfig配置

--- packages/iam/test.ts
+++ packages/iam/test.ts
@@ -2,7 +2,7 @@ import appInit from "./src/app/main.backend";
 
 const app = appInit();
 
-module.exports = async function(req, res, next) {
+module.exports = async function(req: { url: string; }, res: unknown, next: () => void) {
   if (/^\/api/.test(req.url)) {
     const mid = (await app).getHttpAdapter().getInstance();
     return mid(req, res);
