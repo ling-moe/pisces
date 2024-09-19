4ef20d54d68f8ab0b3d3556e5b451d2d5aa541ba
dawn-book <yukdawn@gmail.com>
2024-09-19 17:35:09 +0800

    add: 添加一点生成代码工具

--- libs/musubi/schematics/code-cdtr/index.ts
+++ libs/musubi/schematics/code-cdtr/index.ts
@@ -1,6 +1,7 @@
 import { externalSchematic, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { runInNewContext } from "node:vm";
 import { select, input, multi, number, group } from './form-builder';
+import { ast, query } from '@phenomnomnominal/tsquery';
 
 type Option = {
   code: string;
@@ -16,14 +17,7 @@ export function codeCdtr(_options: Option): Rule {
     const result = runInNewContext(code,{ select, input, multi, number, group,});
     // TODO 暂时只支持多文件组件
     if(tree.exists(`${componentUri}/${componentName}.component.ts`)){
-      const html = tree.get(`${componentUri}/${componentName}.component.html`);
-      const ts = tree.get(`${componentUri}/${componentName}.component.ts`);
-      const htmlRecoder = tree.beginUpdate(`${componentUri}/${componentName}.component.html`);
-      const tsRecoder = tree.beginUpdate(`${componentUri}/${componentName}.component.ts`);
-      htmlRecoder.insertLeft(0, result[0].template);
-      tsRecoder.insertLeft(0, result[0].init);
-      tree.commitUpdate(htmlRecoder)
-      tree.commitUpdate(tsRecoder)
+      applyCode(tree, componentUri, componentName, result);
     }else{
       externalSchematic('@schematics/angular', 'component', {
         name: componentName,
@@ -34,3 +28,23 @@ export function codeCdtr(_options: Option): Rule {
     return tree;
   };
 }
+
+async function applyCode(tree: Tree, componentUri: string, componentName: string, result: any) {
+  // 解析ts
+  const tsAst = ast(tree.readText(`${componentUri}/${componentName}.component.ts`));
+  const classNode = query(tsAst, 'ClassDeclaration > PropertyDeclaration,Constructor,MethodDeclaration')
+  const firstNode = classNode.find(i => i.getText().includes("form$11")) || classNode[0];
+  firstNode.getStart()
+  const htmlFile = tree.readText(`${componentUri}/${componentName}.component.html`);
+  // 解析模板
+  const angularCompiler = await import('@angular/compiler');
+  const parsedTemplate = angularCompiler.parseTemplate(htmlFile, `${componentName}.component.html`);
+  console.log(parsedTemplate.nodes);
+  const htmlRecoder = tree.beginUpdate(`${componentUri}/${componentName}.component.html`);
+  const tsRecoder = tree.beginUpdate(`${componentUri}/${componentName}.component.ts`);
+  htmlRecoder.insertLeft(0, result[0].template);
+  tsRecoder.insertLeft(firstNode.getStart(), result[0].init);
+  tree.commitUpdate(htmlRecoder);
+  tree.commitUpdate(tsRecoder);
+}
+
